b6e3a305cb1d576b64a662ef0027719ee310df0f
2233admin <1497479966@qq.com>
2026-04-06 07:14:21 +0800

    chore: remove test artifacts left by adversarial agents

--- _stress_wscheck.js
+++
@@ -1,4 +0,0 @@
-const ws = require('ws');
-console.log('ws type:', typeof ws);
-console.log('ws.WebSocket type:', typeof ws.WebSocket);
-console.log('keys:', Object.keys(ws).slice(0,10).join(', '));
